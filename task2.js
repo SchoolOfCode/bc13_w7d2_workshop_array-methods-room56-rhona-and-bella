09d3b07bc49f3c430897071331cbb744a1240f5e
rhonamackay <rhonamackay@outlook.com>
2022-11-08 12:24:12 +0000

    Task 2

--- task2.js
+++ task2.js
@@ -8,4 +8,13 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter( function(animal) {
+  console.log(animal.charAt(0))
+  if (animal.charAt(0) == 'b') {
+    return true
+  } else {
+    return false
+  }
+})
+
+console.log(bListAnimals)
